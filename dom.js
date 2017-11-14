b94763a74a5f91cfbecabecd1f5474bfcf0eae6c
Sultan H <mrsultanassi@gmail.com>
2017-11-14 20:43:44 +0200

    added some codes to addTodo, check it out....

--- dom.js
+++ dom.js
@@ -13,22 +13,40 @@
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
     var todoNode = document.createElement('li');
-    // you will need to use addEventListener
-
-    // add span holding description
-
-    // this adds the delete button
+    var addNodeButton = document.querySelector('input[type="submit"]');
+    // Declaring a DELETE button with DELETE text inside of it.
     var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
-      var newState = todoFunctions.deleteTodo(state, todo.id);
-      update(newState);
-    });
-    todoNode.appendChild(deleteButtonNode);
+      deleteButtonNode.innerText = "delete";
+    // Declaring a MARK button with MARK text inside of it.
+    var markNodeButton = document.createElement('button');
+      markNodeButton.innerText = "mark";
+    // This is the SUBMIT button, which does everything!!
+      addNodeButton.addEventListener('click', function(event){
+    // event.preventDefault is to prevent browser NORMAL FUNCTIONS from executing....
+          event.preventDefault();
+    // Declaring a string to retrieve the TEXT from our input field after the SUBMIT click.
+        var nodeDescription = document.querySelector('input[name="description"]').value;
+    // Declaring An array to hold the value of the Returned array from the function addToDo
+    // we will pass the STATE array, and the nodeDescription....
+        var newNodesArray = todoFunctions.addTodo(state, {description: nodeDescription});
+    // Retrieving the values from the Object we created in the new array(newNodesArray).
+          todoNode.id = newNodesArray[newNodesArray.length-1].id;
+          todoNode.innerText = newNodesArray[newNodesArray.length-1].description;
+          todoNode.className = "not-checked"; // this will be the normail class when DONE is false.
+          todoNode.appendChild(deleteButtonNode); // add deleteButtonNode to the IL element we creating.
+          todoNode.appendChild(markNodeButton); // add markNodeButton to the IL element we creating.
+        console.log(todoNode) // testing :D
+      });
+
+    // Add event Listener to deleteButtonNode... still have some work to do...
+      deleteButtonNode.addEventListener('click', function(event){
+        var newState = todoFunctions.deleteTodo(state, todo.id);
+        update(newState);
+      });
 
     // add markTodo button
 
     // add classes for css
-
     return todoNode;
   };
 
@@ -36,9 +54,7 @@
   if (addTodoForm) {
     addTodoForm.addEventListener('submit', function(event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-      // what does event.preventDefault do?
-      // what is inside event.target?
-
+      event.preventDefault();
       var description = '?'; // event.target ....
 
       // hint: todoFunctions.addTodo
