3647b892253f170f081ecf2f708f308a9ff49918
Tariq B. Al-Qazzaz <31041057+tqazaz@users.noreply.github.com>
2017-11-18 09:49:48 +0200

    alltests are seperated Relates #24

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
+test('Add test', function(t) {
   t.deepEqual(logic.addTodo([],{description:"screen"}),[{id:1, description:"screen", done:false}], "test1: actual should be equal to expected");
   t.deepEqual(logic.addTodo([],{description:"salam"}),[{id:2, description: "salam", done:false}], "test2: actual should be equal to expected");
+
+
+  t.end();
+});
+
+test('Delete test', function(t) {
+
 
   t.deepEqual(logic.deleteTodo([{id: 1 , description: " HI " , done: false},
                                 {id: 2 , description: "Hello" , done: false}],
@@ -11,12 +18,25 @@ test('Example test', function(t) {
                                [{id: 2 , description: "Hello" , done: false}],
                                "Only Object 2.");
 
+  t.end();
+});
+
+
+test('Edit test', function(t) {
+
   t.deepEqual(logic.markTodo([{id: 1 , description: " HI " , done: false},
                               {id: 2 , description: "Hello" , done: false}],
                                    2),
                              [{id: 1 , description : " HI " , done: false},
                               {id: 2 , description : "Hello" , done: true}],
                               "2 Must be true.");
+
+  t.end();
+});
+
+
+test('sort  test', function(t) {
+
   t.deepEqual(logic.sortTodos([{id: 1 , description: "HI" , done: true},
                               {id: 2 , description: "Hello" , done: false}]),
                              [{id: 2 , description : "Hello" , done: false},
